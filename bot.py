c0c8e00a16ff972c1d3ccef9fb05136a1aca7134
Neto Santos <fernandobezerrados@gmail.com>
2026-04-27 11:36:37 -0300

    correção no link do localhost

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-N8N_URL = os.getenv("N8N_URL", "http://localhost:5678/webhook/teste")
+N8N_URL = os.getenv("N8N_URL", "http://n8n:5678/webhook/teste")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Olá! Sou o TeleTony. Como posso ajudar?")
